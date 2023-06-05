f6e06b7b217d22356ffd04cacd301b7351b86d6e
Lucas <lucas.leblanc@orange.fr>
2023-06-05 17:54:32 +0200

    News display + member icons

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import { List, ListItem, ListItemText, Divider } from '@mui/material';
+import { List, ListItem, ListItemText, Divider, Stack } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 // import { render } from "@testing-library/react";
@@ -14,6 +15,12 @@ const listStyle = {
     maxWidth: 360,
     bgcolor: 'background.paper',
   };
+
+const newStyle = {
+    backgroundColor: '#f6f7ff',
+    padding: '10px'
+};
+    
 
 class CalendarDisplay extends Component {
   state = {
@@ -30,31 +37,34 @@ class CalendarDisplay extends Component {
 
   render() {
     return (
-      <Grid container spacing={2}>
-        <Grid item xs={1}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-        </Grid>
-        <Grid item xs={11}>
-          <Grid xs display="flex">
-            <Grid item>
-              <div className="calendarStyle">
-                <Calendar
-                  localizer={localizer}
-                  defaultDate={new Date()}
-                  defaultView="month"
-                  events={this.state.events}
-                  style={{ height: "50vh" }}
-                />
-              </div>
-            </Grid>
-          </Grid>
-        </Grid >
-      </Grid >
+        <>
+            <h2>Calendar</h2>
+            <Grid container spacing={2}>
+                <Grid item xs={1}
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+                >
+                </Grid>
+                <Grid item xs={11}>
+                <Grid xs display="flex">
+                    <Grid item>
+                    <div className="calendarStyle">
+                        <Calendar
+                        localizer={localizer}
+                        defaultDate={new Date()}
+                        defaultView="month"
+                        events={this.state.events}
+                        style={{ height: "50vh" }}
+                        />
+                    </div>
+                    </Grid>
+                </Grid>
+                </Grid >
+            </Grid >
+      </>
     );
   }
 }
@@ -70,37 +80,68 @@ function MemberDisplay() {
 
     
     return (
-        <div>
-            <h1>Members</h1>
+        <>
+            <h2>Members</h2>
             <List sx={listStyle}>
                 {Members.map ((member) => (
                     <>
                         <ListItem>
+                            <AccountCircleIcon />
                             <ListItemText primary={member.name} />
                         </ListItem>
                         <Divider />
                     </>
                 ))}
             </List>
-        </div>
+        </>
     );
 }
 
+function NewsDisplay() {
 
+    const news = [
+        { title: 'News 1', content: 'News 1 content' },
+        { title: 'News 2', content: 'News 2 content' },
+    ];
+
+    return (
+        <>
+            <h2>News</h2>
+            <Stack 
+                direction="row-reverse"
+                spacing={2}
+                sx={{ width: '100%' }}
+                divider={<Divider orientation="vertical" flexItem />}
+            >
+                {news.map((news) => (
+                    <>
+                        <div style={newStyle}>
+                            <h3>{news.title}</h3>
+                            <p>{news.content}</p>
+                        </div>
+                    </>
+                ))}
+            </Stack>
+        </>
+    );
+}
 
 const Dashboard = () => {
     return (
-        <div>
+        <>
             <h1>Dashboard</h1>
             <Grid container spacing={2}>
-                <Grid xs={10}>
+                <Grid xs={9}>
                     <CalendarDisplay />
                 </Grid>
-                <Grid xs={2}>
+                <Grid xs spacing={10}>
                     <MemberDisplay />
                 </Grid>
+                <Grid xs={12}>
+                    <NewsDisplay />
+                </Grid>
             </Grid>
-        </div>
+        </>
     )
 }
 
